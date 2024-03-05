12d9e0bfa94331959ea4112b74c32f2e5a9d444d
pepormaty <themranti3256@gmail.com>
2024-03-06 01:44:39 +0700

    update url

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,11 @@ import react from "@vitejs/plugin-react";
 import type { UserConfig as VitestUserConfigInterface } from "vitest/config";
 
 const vitestConfig: VitestUserConfigInterface = {
-  base: "rickandmorty",
+  base: "/rickandmorty/",
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
-    // you might want to disable it, if you don't have tests that rely on CSS
-    // since parsing CSS is slow
     css: true,
   },
 };
